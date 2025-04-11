fe6d5914e8c9c3da8f45c67482d939c3b2d1e9a5
Jacobs2Jacob <jacobs2jacob@gmail.com>
2025-04-11 21:37:52 +0300

    .

---
+++ api/cocktails/index.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+
+module.exports = async function (context, req) {
+    const path = context.bindingData.proxy || "";
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/${path}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        context.res = {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: data
+        };
+    } catch (err) {
+        context.res = {
+            status: 500,
+            body: { error: "Proxy fetch failed", message: err.message }
+        };
+    }
+};
