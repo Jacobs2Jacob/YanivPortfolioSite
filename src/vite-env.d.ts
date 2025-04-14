7aeac445a4b3fa770705dee4c19a8bb738a2b0b7
Jacobs2Jacob <jacobs2jacob@gmail.com>
2025-04-15 00:09:12 +0300

    Add interface to vite varibles.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_COCKTAIL_API_URL: string;
+  readonly VITE_COCKTAILS_STORAGE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
