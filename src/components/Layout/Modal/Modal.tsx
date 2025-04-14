64427ef81b251531f06e6923dac88bf89c8b4d83
Jacobs2Jacob <jacobs2jacob@gmail.com>
2025-04-14 23:32:12 +0300

    Wrap modal with portal.

--- src/components/Layout/Modal/Modal.tsx
+++ src/components/Layout/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Modal.module.css';
+import ReactDOM from 'react-dom';
 
 interface ModalProps {
     isOpen: boolean;
@@ -12,13 +13,14 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
     if (!isOpen) {
         return null;
     } 
-
-    return (
+     
+    return ReactDOM.createPortal(
         <div className={styles.overlay} onClick={onClose}>
             <div className={styles.modal} onClick={e => e.stopPropagation()}>
                 {children}
             </div>
-        </div>
+        </div>,
+        document.getElementById('modal-root')!
     );
 };
 
