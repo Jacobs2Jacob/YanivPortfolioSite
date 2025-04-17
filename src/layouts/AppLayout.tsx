65c8ce394f8f190287432abcb570a836b5b4e562
Jacobs2Jacob <30956957+Jacobs2Jacob@users.noreply.github.com>
2025-04-17 08:07:35 +0300

    Update AppLayout.tsx

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Layout/Navbar/Navbar'; 
 import { useModal } from '../contexts/ModalContext';
 import Loader from '../components/Layout/Loader/Loader';
+import { SearchBarProvider } from '../contexts/SearchBarContext';
 
 const LazyModal = lazy(() => import('@/components/Layout/Modal/Modal'));
 
@@ -10,13 +11,14 @@ const AppLayout: React.FC = () => {
     
     const { closeModal, isOpen, content } = useModal();
 
-    
     return (
         <>
             <Navbar />
 
             <main>
-                <Outlet />
+                <SearchBarProvider>
+                    <Outlet />
+                </SearchBarProvider>
             </main>
 
             <Suspense fallback={<Loader />}>
