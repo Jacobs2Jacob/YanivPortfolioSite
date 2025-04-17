9b46c7b38dfe868d55103e6310a95024dda97639
Jacobs2Jacob <30956957+Jacobs2Jacob@users.noreply.github.com>
2025-04-17 07:37:53 +0300

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,14 @@
-﻿import React from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router';
 import CocktailNavigator from '@/features/cocktails/components/CocktailNavigator/CocktailNavigator';
 
 const HomePage: React.FC = () => {
+
+    const navigate = useNavigate();
+
     return (
         <>
+            <SearchBar onSelect={(id) => navigate(`/cocktail/${id}`)} />
             <CocktailNavigator />
         </>
     );
