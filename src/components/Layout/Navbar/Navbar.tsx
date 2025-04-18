8c3a972ae7d441a743e03f9940f4bdb9eb6857d0
Jacobs2Jacob <30956957+Jacobs2Jacob@users.noreply.github.com>
2025-04-18 07:12:47 +0300

    Update Navbar.tsx

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,14 +1,12 @@
-﻿import React from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Navbar.module.css';
-import { useDeviceDetection } from '@/hooks/useDeviceDetection';
 
 const Navbar: React.FC = () => {
     const navigate = useNavigate();
-    const device = useDeviceDetection();
 
     return (
-        device === 'desktop' ? <nav className={styles.navbar}>
+        <nav className={styles.navbar}>
             <div className={styles.container}>
                 <button onClick={() => navigate('/')} className={styles.navButton}>
                     Home
@@ -19,18 +17,6 @@ const Navbar: React.FC = () => {
                 <h1 className={styles.title}>
                     COCKTAIL GALLERY
                 </h1>
-            </div>
-        </nav> : <nav className={styles.navbar}>
-            <div className={styles.container}>
-                <button onClick={() => navigate('/')} className={styles.navButton}>
-                    Home
-                </button>
-                <h1 className={styles.title}>
-                    COCKTAIL GALLERY
-                </h1>
-                <button onClick={() => navigate('/cocktail/new')} className={styles.navButton}>
-                    🍸 New Cocktail
-                </button>
             </div>
         </nav>
     );
